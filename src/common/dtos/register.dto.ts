ce43e14b0a78affb5e5610e8abee63ccaef4d673
Tahir Mahmudzade <tahirmh41@gmail.com>
2023-08-21 11:52:57 +0400

    Fixes

--- src/common/dtos/register.dto.ts
+++ src/common/dtos/register.dto.ts
@@ -1,7 +1,19 @@
-import { IsEmail, IsOptional, IsString, IsUrl } from 'class-validator';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterDto {
-  @IsEmail({ allow_utf8_local_part: true }, { message: 'Invalid email format' })
+  @IsEmail({ allow_utf8_local_part: true }, { message: 'Invalid email' })
+  @MaxLength(255, {
+    message(validationArguments) {
+      return `Email is too long. Maximal length is ${validationArguments.constraints[0]} characters`;
+    },
+  })
   public email: string;
 
   @IsString({ message: 'First name must be a string' })
@@ -10,7 +22,17 @@ export class RegisterDto {
   @IsString({ message: 'Last name must be a string' })
   public lastName: string;
 
-  @IsString({ message: 'Password must be a string' })
+  @IsString({ message: 'Invalid password' })
+  @MaxLength(20, {
+    message(validationArguments) {
+      return `Password is too long. Maximal length is ${validationArguments.constraints[0]} characters`;
+    },
+  })
+  @MinLength(8, {
+    message(validationArguments) {
+      return `Password is too short. Minimal length is ${validationArguments.constraints[0]} characters`;
+    },
+  })
   public password: string;
 
   @IsString({ message: 'Path to the actual image must be a string' })
