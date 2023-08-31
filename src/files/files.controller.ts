597eb848f374e1e8b7296e5b199c1cda8ed7dad1
Tahir Mahmudzade <tahirmh41@gmail.com>
2023-08-31 21:53:29 +0400

    Updates

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -13,15 +13,12 @@ import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadFilePipe } from 'src/common/pipes/uploadFilePipe.pipe';
 import { Request } from 'express';
-import { AdminGuard } from 'src/common/guards/admin.guard';
-import { AuthGuard } from 'src/common/guards/auth.guard';
 import { AccessTokenGuard } from 'src/common/guards/accessToken.guard';
 import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Files')
 @ApiBearerAuth('Access Token')
 @UseGuards(AccessTokenGuard)
-@UseGuards(AuthGuard)
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
@@ -68,27 +65,23 @@ export class FilesController {
     return uploadedImage;
   }
 
-  @UseGuards(AdminGuard)
   @Get('retrieveAllFiles')
   async retrieveAllFiles() {
     const files = await this.filesService.retrieveFiles();
     return files;
   }
 
-  @UseGuards(AdminGuard)
   @Get('retrieveFiles/:id')
   async retrieveUserFiles(@Param('id') id: string) {
     const files = await this.filesService.retrieveFiles(parseInt(id));
     return files;
   }
 
-  @UseGuards(AdminGuard)
   @Delete('deleteAllFiles')
   async deleteAllFiles() {
     return this.filesService.deleteFiles();
   }
 
-  @UseGuards(AdminGuard)
   @Delete('deleteFiles/:id')
   async deleteUserFiles(@Param('id') id: string) {
     return this.filesService.deleteFiles(parseInt(id));
